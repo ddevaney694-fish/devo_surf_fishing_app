0ee23a6f3d0c369df2725f6e0026f8cc0b7df5ec
ddevaney694-fish <ddevaney694@gmail.com>
2026-09-08 09:21:11 -0700

    Update solunar_page.py

--- solunar_page.py
+++ solunar_page.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from solunar_tools import get_solunar_data
 
 def storm_card(title, value):
     st.markdown(
@@ -15,24 +16,21 @@ def render():
     st.markdown("<h1 style='color:#58a6ff;'>Solunar Forecast</h1>", unsafe_allow_html=True)
     st.markdown("<div class='wave-divider'></div>", unsafe_allow_html=True)
 
-    # Placeholder values (real solunar engine coming later)
-    moon_phase = "Waning Gibbous"
-    major_window = "10:45 AM – 12:15 PM"
-    minor_window = "5:20 PM – 6:05 PM"
-    bite_rating = "GOOD"
+    # Get real solunar data
+    data = get_solunar_data()
 
     col1, col2, col3 = st.columns(3)
     with col1:
-        storm_card("Moon Phase", moon_phase)
+        storm_card("Moon Phase", data["moon_phase"])
     with col2:
-        storm_card("Major Bite Window", major_window)
+        storm_card("Major Bite Window", data["major_window"])
     with col3:
-        storm_card("Minor Bite Window", minor_window)
+        storm_card("Minor Bite Window", data["minor_window"])
 
     st.markdown("<div class='wave-divider'></div>", unsafe_allow_html=True)
 
-    storm_card("Bite Rating", bite_rating)
+    storm_card("Bite Rating", data["bite_rating"])
 
     st.markdown("<div class='wave-divider'></div>", unsafe_allow_html=True)
 
-    st.info("Solunar engine will be connected soon for live bite windows and moon phase data.")
+    st.info("Solunar engine is now active and generating live bite windows and moon phase data.")
